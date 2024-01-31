07193f991d7cc2e8c3bc88baad7a264012561a59
Ethem Serinoglu <132064533+eserinoglu@users.noreply.github.com>
2024-01-31 11:51:53 +0300

    .

--- supabase/supabase.js
+++ supabase/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ihmsucjyfbgippgjcfnp.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlobXN1Y2p5ZmJnaXBwZ2pjZm5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIyMTQwMTcsImV4cCI6MjAxNzc5MDAxN30.bno56hEiQbC7GsY3hqqKS2P9qELbElnaPMFNGcHyRLo";
+const supabaseUrl = "YOUR_SUPABASE_URL";
+const supabaseKey = "YOUR_SUPABASE_KEY";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
